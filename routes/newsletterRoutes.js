0ff4626a3cb38197aa7c7da0f7a560212d01e7e4
Leon-kings <leonakingeneye2002@gmail.com>
2026-01-16 22:27:13 +0200

    changes on newsletter

--- routes/newsletterRoutes.js
+++ routes/newsletterRoutes.js
@@ -1,11 +1,39 @@
+// const express = require('express');
+// const router = express.Router();
+// const newsletterController = require('../controllers/newsletter.controller');
+
+// // GET all subscriptions
+// router.get('/', newsletterController.getAll);
+
+// // CREATE subscription
+// router.post('/subscribe', newsletterController.subscribe);
+
+// module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const express = require('express');
 const router = express.Router();
 const newsletterController = require('../controllers/newsletter.controller');
 
-// GET all subscriptions
+// Newsletter
+router.post('/subscribe', newsletterController.subscribe);
 router.get('/', newsletterController.getAll);
 
-// CREATE subscription
-router.post('/subscribe', newsletterController.subscribe);
+// Statistics
+router.get('/statistics', newsletterController.getStatistics);
 
 module.exports = router;
